3300001d29365a82f242ea6d506d36b688db612c
Dimitris <iracleous@gmail.com>
2020-04-25 11:46:40 +0300

    Add project files.

---
+++ CrmApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CrmApp
+{
+      class AnotherClass
+    {
+         static void Main()
+        {
+            string returnProduct;
+            string message = "Give the name of the product";
+            //method invocation or method call or method usage
+            returnProduct =  DoWork(message);
+            Console.WriteLine(returnProduct);
+        }
+
+        //declaration of a method
+        static string DoWork(string message)
+        {
+            string product;
+            Console.WriteLine(message);
+            product = Console.ReadLine();
+            return product;
+        }
+
+    }
+}
